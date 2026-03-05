e513f4a7b07dee8654854da769158cf99abb5bca
anmurog <anmurog@unirioja.es>
2026-03-05 12:34:07 +0100

    Update AnnealingSearch.java

--- hipster-core/src/main/java/es/usc/citius/hipster/algorithm/localsearch/AnnealingSearch.java
+++ hipster-core/src/main/java/es/usc/citius/hipster/algorithm/localsearch/AnnealingSearch.java
@@ -59,6 +59,8 @@ public class AnnealingSearch<A, S, N extends HeuristicNode<A, S, Double, N>> ext
 	static final private Double DEFAULT_MIN_TEMP = 0.00001;
 	static final private Double START_TEMP = 1.;
 
+	static final private Random randIndGen = new Random();
+
 	private N initialNode;
 	private Double alpha;
 	private Double minTemp;
@@ -116,7 +118,7 @@ public class AnnealingSearch<A, S, N extends HeuristicNode<A, S, Double, N>> ext
 					for (N successor : nodeExpander.expand(node)) {
 						successors.add(successor);
 					}
-					Random randIndGen = new Random();
+			
 					return successors.get(Math.abs(randIndGen.nextInt()) % successors.size());
 				}
 			};
@@ -201,3 +203,4 @@ public class AnnealingSearch<A, S, N extends HeuristicNode<A, S, Double, N>> ext
 		N estimate(N node, NodeExpander<A, S, N> nodeExpander);
 	}
 }
+
